673688ab453f6d03a5949312378d49b0301149e3
Dmitry Makarov <makdmt@gmail.com>
2024-05-06 17:01:37 +0300

    feat (task 1) write docode function

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,28 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
 console.log("Let's rock")
 console.log(encoded, translations)
 
 
+function decode(encArr, glossary) {
+    const ids = new Set()
 
-// console.log(decoded)
+    const decodedArr = encArr.map(({ mediaId, groupId, service, formatSize, ca }) => {
+        ids.add(mediaId);
+        const decodedElm = {
+            mediaId: glossary[mediaId],
+            groupId,
+            service,
+            formatSize,
+            ca
+        }
+        return decodedElm;
+    })
+
+    return ({
+        uniqueIds: Array.from(ids),
+        decoded: decodedArr
+    })
+}
+
+console.log(decode(encoded, translations))
